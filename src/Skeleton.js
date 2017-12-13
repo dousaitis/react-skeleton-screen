bae20902e35dd28a430db27053b531af354709b9
johndous <johndous@hotmail.com>
2017-12-14 00:59:25 +0200

    Add marginTop, marginRight, marginBottom, marginLeft as props

--- src/Skeleton.js
+++ src/Skeleton.js
@@ -5,18 +5,31 @@ class Skeleton extends Component {
   static defaultProps = {
     width: '100%',
     height: '100%',
-  }
+    marginTop: '',
+    marginRight: '',
+    marginBottom: '', 
+    marginLeft: '',
+  };
   static propTypes = {
     width: PropTypes.string,
     height: PropTypes.string,
-  }
+    marginTop: PropTypes.string,
+    marginRight: PropTypes.string,
+    marginBottom: PropTypes.string,
+    marginLeft: PropTypes.string,
+  };
   render() {
-    const { width, height } = this.props
+    const { width, height, marginTop, marginRight, marginBottom, marginLeft } = this.props
+    const style = {
+      ...width && { width },
+      ...height && { height },
+      ...marginTop && { marginTop },
+      ...marginRight && { marginRight },
+      ...marginBottom && { marginBottom },
+      ...marginLeft && { marginLeft },
+    }
     return (
-      <div
-        className="Skeleton"
-        style={{ width, height }}
-      >
+      <div className="Skeleton" style={style}>
         <div className="Skeleton-body" />
       </div>
     )
--- src/__tests__/Skeleton-test.js
+++ src/__tests__/Skeleton-test.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom'
 import ReactDOMServer from 'react-dom/server'
 import Skeleton from '../Skeleton'
 
-describe("A <Skeleton>", () => {
+describe('A <Skeleton>', () => {
   let node
   beforeEach(() => {
-    node = document.createElement("div")
+    node = document.createElement('div')
   })
 
-  describe("rendered on client", () => {
+  describe('rendered on client', () => {
   
-    it("renders with width and height", () => {
+    it('renders with width and height', () => {
       const element = (
         <Skeleton width="50%" height="40px" />
       )
@@ -22,17 +22,37 @@ describe("A <Skeleton>", () => {
         expect(node.firstChild.style.height).toMatch(/40px/)
       })
     })
+
+    it('renders with marginTop, marginRight, marginBottom and marginLeft', () => {
+      const element = (
+        <Skeleton marginTop="10px" marginRight="20px" marginBottom="30px" marginLeft="40px" />
+      )
+
+      ReactDOM.render(element, node, () => {
+        expect(node.firstChild.style.marginTop).toMatch(/10px/)
+        expect(node.firstChild.style.marginRight).toMatch(/20px/)
+        expect(node.firstChild.style.marginBottom).toMatch(/30px/)
+        expect(node.firstChild.style.marginLeft).toMatch(/40px/)
+      })
+    })
     
   })
   
-  describe("rendered on the server", () => {
+  describe('rendered on the server', () => {
     
-    it("renders with width and height", () => {
+    it('renders with width and height', () => {
       const markup = ReactDOMServer.renderToStaticMarkup(
         <Skeleton width="50%" height="40px" />
       )
 
       expect(markup).toMatch(/<div class="Skeleton" style="width:50%;height:40px"><div class="Skeleton-body"><\/div><\/div>/)
+    })
+
+    it("renders with marginTop, marginRight, marginBottom and marginLeft", () => {
+      const markup = ReactDOMServer.renderToStaticMarkup(
+        <Skeleton marginTop="10px" marginRight="20px" marginBottom="30px" marginLeft="40px" />
+      )
+      expect(markup).toMatch(/<div class="Skeleton" style="width:100%;height:100%;margin-top:10px;margin-right:20px;margin-bottom:30px;margin-left:40px"><div class="Skeleton-body"><\/div><\/div>/)
     })
   })
 })
